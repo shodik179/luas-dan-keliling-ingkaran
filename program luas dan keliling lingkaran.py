5354bbeda73fcce818b8f0a1905fb4b8fa89ad1a
shodik179 <shodik179@gmail.com>
2021-11-04 19:14:00 +0700

    menambahkan file

---
+++ program luas dan keliling lingkaran.py
@@ -0,0 +1,9 @@
+print("program menghitung luas dan keliling lingkaran")
+r = int (input("masukan jari - jari "))
+
+phi = 22/7
+luas = phi*r*r
+kel = 2*phi*r
+
+print ("luas lingkaran adalah      =", luas)
+print ("keliling lingkaran adalah  =", kel)
